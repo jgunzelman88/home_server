c8bd85764f804fa9b22613ca6e62b722963c6f76
jgunzelman <jgunzelman88@gmail.com>
2023-03-17 18:47:43 -0400

    Add yml and lates to dockerfile

--- poketbase/build.sh
+++ poketbase/build.sh
@@ -1,3 +1,3 @@
 #/etc/sh
 export POCKETBASE_VERSION="0.13.4"
-docker build --build-arg $POCKETBASE_VERSION -t pocketbase:$POCKETBASE_VERSION .
+docker build --build-arg $POCKETBASE_VERSION -t pocketbase:$POCKETBASE_VERSION -t latest .
