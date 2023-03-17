5917c486f9d2494f8c6c465eda275fc5d2a02ff9
jgunzelman <jgunzelman88@gmail.com>
2023-03-17 18:49:26 -0400

    🐛 fix tag

--- poketbase/build.sh
+++ poketbase/build.sh
@@ -1,3 +1,3 @@
 #/etc/sh
 export POCKETBASE_VERSION="0.13.4"
-docker build --build-arg $POCKETBASE_VERSION -t pocketbase:$POCKETBASE_VERSION -t latest .
+docker build --build-arg $POCKETBASE_VERSION -t pocketbase:$POCKETBASE_VERSION -t pocketbase:latest .
