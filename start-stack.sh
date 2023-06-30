e0b9dffbd80c98a7873ffb98a25e70e511a2eced
jgunzelman <jgunzelman88@gmail.com>
2023-06-30 13:07:52 -0400

    🐛

--- start-stack.sh
+++ start-stack.sh
@@ -1,7 +1,6 @@
 STATUS=true
 SERVICE="$1"
 CONFIG="$2"
-SLEEP_INT=3600
 #Start docker stack
 sudo docker stack deploy -c $CONFIG $SERVICE
 #check status periodically
@@ -10,6 +9,6 @@ do
     status_output="$(sudo docker stack ps -f desired-state=running $SERVICE)"
     if ["$status_output" != "nothing found in stack: $SERVICE"]; then
         STATUS=false
-        sleep("$SLEEP_INT")
+        sleep 3600
     fi
 done
