7b57b859707e97df21d3c6d2def5c1cf0d2a3b87
jgunzelman88 <jgunzelman88@gmail.com>
2026-06-27 11:49:40 -0400

    🐛

--- helm/init-n8n.sh
+++ helm/init-n8n.sh
@@ -2,6 +2,6 @@
 #!/bin/bash
 kubectl create namespace n8n
 
-helm install --install n8n ./n8n \
+helm upgrade --install n8n ./n8n \
   --namespace n8n \
   --set n8n.encryptionKey="$(openssl rand -hex 32)"
